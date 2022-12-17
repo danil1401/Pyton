ca695d39ecf578db9adc04d1644284ba35c0cc54
danil1401 <danil1401@yandex.ru>
2022-12-17 12:23:59 +0400

    Начало семинара 2

--- Seminar1/HomeWork1.py
+++ Seminar1/HomeWork1.py
@@ -89,16 +89,16 @@
 # - A (3,6); B (2,1) -> 5,09
 # - A (7,-5); B (1,-1) -> 7,21
 
-while (True):
-    try:
-       a=list(map(float,input('insert A(x1,y1), for example: "3 whitespace 6" : ').split()))
-       break
-    except Exception: print('Error: Check your value!')
-while (True):    
-    try:
-       b=list(map(float,input('insert B(x2,y2), for example: "2 whitespace 1" : ').split()))
-       break
-    except Exception: print('Error: Check your value!')
-print(f'Distanse between A{a} and B{b} is: {round(((a[0]-b[0])**2+(a[1]-b[1])**2)**0.5,3)}')
+# while (True):
+#     try:
+#        a=list(map(float,input('insert A(x1,y1), for example: "3 whitespace 6" : ').split()))
+#        break
+#     except Exception: print('Error: Check your value!')
+# while (True):    
+#     try:
+#        b=list(map(float,input('insert B(x2,y2), for example: "2 whitespace 1" : ').split()))
+#        break
+#     except Exception: print('Error: Check your value!')
+# print(f'Distanse between A{a} and B{b} is: {round(((a[0]-b[0])**2+(a[1]-b[1])**2)**0.5,3)}')
 
 
