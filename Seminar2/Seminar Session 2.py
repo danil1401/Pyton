1400c72fffbef1fc0ce95d3a244c053d81521e72
danil1401 <danil1401@yandex.ru>
2022-12-17 13:10:09 +0400

    Пауза Семинар 2-продолжить с 1:09

--- Seminar2/Seminar Session 2.py
+++ Seminar2/Seminar Session 2.py
@@ -1,4 +1,4 @@
-#                                                           Проверка ДЗ
+#                                                           Проверка ДЗ(интересные альтернативные решения)
 # Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
 
 # num=int(input('Write day of week '))
@@ -14,12 +14,16 @@
 # ⋁-or
 # ¬-not
 
-for x in[True,False]:
-    for y in [True, False]:
-        for z in [True, False]:
-            print(f'x={x} y={y} z={z}', end='->')
-            print( not(x and y and z)==(not x) or (not y) or (not z))
-#                 result='Верно'
-#             else:
-#                 result='Ложно'
-# print(result)
+# for x in[True,False]:
+#     for y in [True, False]:
+#         for z in [True, False]:
+#             print(f'x={x} y={y} z={z}', end='->')
+#             print( not(x and y and z)==(not x) or (not y) or (not z))
+# #                 result='Верно'
+# #             else:
+# #                 result='Ложно'
+# # print(result)
+
+# Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
+print('диаопозон для x и y')
+values=['x > 0 y < 0', 'x < 0 y > )', 'x < 0 y < 0', 'x > 0 y < 0']
