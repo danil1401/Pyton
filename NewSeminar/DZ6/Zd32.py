1211c8b2f125f3e758cdbef43e14b3b1910059b8
danil1401 <danil1401@yandex.ru>
2023-03-14 21:28:20 +0400

    Конец Дз 6

--- NewSeminar/DZ6/Zd32.py
+++ NewSeminar/DZ6/Zd32.py
@@ -5,3 +5,15 @@
 # Ввод: [-5, 9, 0, 3, -1, -2, 1, 4, -2, 10, 2, 0, -9, 8, 10, -9, 0, -5, -5, 7]
 # Вывод: [1, 9, 13, 14, 19]
 
+from random import randint
+
+n = int(input('n = '))
+my_list = [randint(-10,10) for _ in range(n)]
+print(my_list)
+print(list(enumerate(my_list)))
+min_ = int(input('min = '))
+max_ = int(input('max = '))
+
+for i in range(n):
+    if max_ >= my_list[i] >= min_:
+        print(i, end=' ')
