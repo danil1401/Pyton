2af455bcb089f964908e967bc708a4a2f743d399
danil1401 <danil1401@yandex.ru>
2023-03-14 21:39:38 +0400

    Добавлены эталоны решения по ДЗ 6

--- NewSeminar/DZ6/Zd32.py
+++ NewSeminar/DZ6/Zd32.py
@@ -17,3 +17,12 @@ max_ = int(input('max = '))
 for i in range(n):
     if max_ >= my_list[i] >= min_:
         print(i, end=' ')
+
+        # ЭТАЛОН РЕШЕНИЯ
+
+list_1 = [-5, 9, 0, 3, -1, -2, 1, 4, -2, 10, 2, 0, -9, 8, 10, -9, 0, -5, -5, 7]
+min_number = int(input())
+max_number = int(input())
+for i in range(len(list_1)):
+    if min_number <= list_1[i] <= max_number:
+        print(i)
