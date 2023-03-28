74ce4af81233649af6b1c6f3aa43a2e881f56386
Danil <urusov.danil1401@yandex.ru>
2023-03-28 23:28:56 +0400

    Семинар 9 продолжить с 44:42

---
+++ NewSeminar/Seminar9.py
@@ -0,0 +1,9 @@
+#                                                         # Урок 9. Работа с табличными данными
+
+# https://drive.google.com/drive/search?q=owner:me%20(type:application/vnd.google.colaboratory%20||%20type:application/vnd.google.colab)
+
+
+# Для работы можно использовать:
+# ГуглКолаб,jupyterlab, либо в ВС коде создавать файл с расширением .ipynb (jupyter notebook)
+
+# ПРОДОЛЖИТЬ СЕМИНАР С 44:42
