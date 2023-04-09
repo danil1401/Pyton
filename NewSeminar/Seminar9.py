d39276c4b206385c920e31fc8c8bed7cd047ef49
danil1401 <danil1401@yandex.ru>
2023-04-09 14:16:54 +0400

    Конец семинара 9

--- NewSeminar/Seminar9.py
+++ NewSeminar/Seminar9.py
@@ -1,9 +1,11 @@
-#                                                         # Урок 9. Работа с табличными данными
+# # #                                                         # Урок 9. Работа с табличными данными
 
-# https://drive.google.com/drive/search?q=owner:me%20(type:application/vnd.google.colaboratory%20||%20type:application/vnd.google.colab)
+# # # https://drive.google.com/drive/search?q=owner:me%20(type:application/vnd.google.colaboratory%20||%20type:application/vnd.google.colab)
 
 
-# Для работы можно использовать:
-# ГуглКолаб,jupyterlab, либо в ВС коде создавать файл с расширением .ipynb (jupyter notebook)
+# # # Для работы можно использовать:
+# # # ГуглКолаб,jupyterlab, либо в ВС коде создавать файл с расширением .ipynb (jupyter notebook)
 
-# ПРОДОЛЖИТЬ СЕМИНАР С 44:42
+# # # ПРОДОЛЖИТЬ СЕМИНАР С 44:42
+# # https://colab.research.google.com/drive/1FwMr6yn_DS8WM60EoV0mEaF-wVD1KLYp?usp=sharing
+# конец семинара
